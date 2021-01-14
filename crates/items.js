3453849b915a4edbe04a27983d973a87faf74204
Gougerik <jakub@webcoder.sk>
2021-01-14 01:55:19 +0100

    Upload crates

---
+++ crates/items.js
@@ -0,0 +1,42 @@
+const items = [
+    {
+        "id":1,
+        "color": "green"
+    },
+    {
+        "id":2,
+        "color": "purple"
+    },
+    {
+        "id":3,
+        "color": "orange"
+    },
+    {
+        "id":4,
+        "color": "blue"
+    },
+    {
+        "id":5,
+        "color": "pink",
+    },
+    {
+        "id":6,
+        "color": "cyan",
+    },
+    {
+        "id":7,
+        "color": "magenta"
+    },
+    {
+        "id":8,
+        "color": "maroon"
+    },
+    {
+        "id":9,
+        "color": "gray"
+    },
+    {
+        "id":10,
+        "color": "yellow"
+    }
+]
---
+++ crates/script.js
@@ -0,0 +1,109 @@
+var sound = new Audio('./sound.mp3');
+var win = new Audio('./win.mp3');
+var roll = new Audio('./roll.mp3');
+const time = 35;
+
+// Shuffle
+
+shuffle = array => {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+  
+    while (0 !== currentIndex) {
+  
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+  
+    return array;
+  }
+
+// Open
+
+$(document).ready(function() {
+    $('.items').css('width',`${items.length * 134 * 4}px`);
+    shuffle(items);
+    for(var i = 0; i < items.length; i++) {
+        $('.items').append(`<div class="item" id="${items[i].id}" style="background-color: ${items[i].color};"></div>`);
+        const sort = items.filter(item => item.id == Number(i)+1);
+        $('.menu').append(`<div class="menuitem" style="background-color: ${sort[0].color}"><span>${sort[0].color}</span></div>`);
+    }
+});
+
+next = () => {
+    $('.items').css('transition',`all 0.2s ease-out 0s`);
+    $('.items').css('transform','translate3d(-206px, 0, 0)');
+    setTimeout(() => {
+        const div = $('.item').first();
+        div.remove();
+        $('.items').append(div);
+        $('.items').css('transition','none');
+        $('.items').css('transform','translate3d(-72px, 0, 0)');
+    }, 60);
+}
+
+finish = () => {
+    const div = $('.items').html();
+    const final = Math.floor(Math.random() * (3000 - 2000)) + 2000;
+    $('.items').append(div);
+    $('.items').append(div);
+    $('.items').css('transition',`all 4s ease-out 0s`);
+    $('.items').css('transform',`translate3d(-${final}px, 0, 0)`);
+    for(var i = 0; i < 8; i++) {
+        setTimeout(() => {
+            sound.play();
+        }, i * 134 * (i/2));
+    }
+    setTimeout(() => {
+        const id = $('.item').eq(Math.round(final/134+2)).attr("id");
+        const reward = items.filter(item => item.id == Number(id));
+        $('#reward').css('background-color',reward[0].color);
+        $('#text').html(`Congratulations.<br>You won <b style="text-transform: uppercase">${reward[0].color}</b> color!`);
+        win.play();
+        $('.modal').css('display','flex');
+        $('.modal').css('opacity',1);
+    }, 4000);
+}
+
+opener = () => {
+    shuffle(items);
+    $('.items').html('');
+    for(var i = 0; i < items.length; i++) {
+        $('.items').append(`<div class="item" id="${items[i].id}" style="background-color: ${items[i].color};"></div>`);
+    }
+    roll.play();
+    $('#init-btn').attr('disabled','true');
+    var i = 0;
+    for(var i = 0; i < time; i++) {
+        setTimeout(() => {
+            next();
+            sound.play();
+        }, i*60);
+    }
+    setTimeout(() => {
+        finish();
+    }, time*60.5);
+}
+// Ripple
+
+const buttons = document.querySelectorAll("[data-animation=\"ripple\"]");
+[...buttons].forEach(e => {
+    e.onmousedown = function(t) {
+        const   e = t.pageX - this.offsetLeft,
+                n = t.pageY - this.offsetTop,
+                o = this.offsetWidth,
+                a = document.createElement("span");
+        a.className = "ripple", a.style.left = e + "px", a.style.top = n + "px", a.style.setProperty("--scale", o), this.appendChild(a), setTimeout(() => {
+            a.parentNode.removeChild(a)
+        }, 500);
+    }
+});
+
+modalclose = () => {
+    $('.modal').css('opacity',0);
+    $('.modal').css('display','none');
+    $('.btn-raised').removeAttr('disabled');
+}
